f0888ec35eebd3f6fd8efe84a3bda960a033b4f5
beyhive16 <raquel.melo.job@gmail.com>
2018-06-03 04:20:25 -0300

    alteracoes 3 de junho

---
+++ js/valida.js
@@ -0,0 +1,17 @@
+function valida() {
+  if (document.div-form1.form1.cpf.validity.patternMismatch) {
+    alert("O CPF está incorreto");
+  } else {
+    alert("O CPF está correto");
+  }
+  return false;
+}
+
+function valida() {
+  if (document.div-form1.form1.telefone.validity.patternMismatch) {
+    alert("O telefone está incorreto");
+  } else {
+    alert("O telefone está correto");
+  }
+  return false;
+}
